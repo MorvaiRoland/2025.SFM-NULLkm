78338a63856edeb8f1df7e27c294426eb7fae5a2
Levente <lorinczlevi0723@gmail.com>
2025-10-10 17:29:23 +0200

    V 0.6 Hasznos Linkek menü átalakítása

--- src/main/java/drivesync/LinksController.java
+++ src/main/java/drivesync/LinksController.java
@@ -2,7 +2,6 @@ package drivesync;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Hyperlink;
-
 import java.awt.Desktop;
 import java.io.IOException;
 import java.net.URI;
@@ -17,23 +16,31 @@ public class LinksController {
     @FXML
     private Hyperlink linkHOLTANKOLJAK;
     @FXML
-
+    private Hyperlink linkWAZE;
+    @FXML
+    private Hyperlink linkGARVISOR;
 
     // Linkekhez tartozó URL-ek
-    private final String URL_HAHU = "https://www.hasznaltauto.hu/";
-    private final String URL_AUTODOC = "https://www.autodoc.hu/jarmu-alkatreszek/fogasszij-keszlet-10553";
-    private final String URL_HOLTANKOLJAK = "https://holtankoljak.hu/#!";
-
+    private static final String URL_HAHU = "https://www.hasznaltauto.hu/";
+    private static final String URL_AUTODOC = "https://www.autodoc.hu/jarmu-alkatreszek/fogasszij-keszlet-10553";
+    private static final String URL_HOLTANKOLJAK = "https://holtankoljak.hu/#!";
+    private static final String URL_WAZE = "https://www.waze.com/hu/live-map/";
+    private static final String URL_GARVISOR = "https://garvisor.com/";
 
     @FXML
     private void initialize() {
         linkHAHU.setOnAction(e -> openLink(URL_HAHU));
         linkAUTODOC.setOnAction(e -> openLink(URL_AUTODOC));
         linkHOLTANKOLJAK.setOnAction(e -> openLink(URL_HOLTANKOLJAK));
-
+        linkWAZE.setOnAction(e -> openLink(URL_WAZE));
+        linkGARVISOR.setOnAction(e -> openLink(URL_GARVISOR));
     }
 
     private void openLink(String url) {
+        if (url == null || url.isBlank()) {
+            return;
+        }
+
         if (Desktop.isDesktopSupported()) {
             try {
                 Desktop.getDesktop().browse(new URI(url));
@@ -42,7 +49,4 @@ public class LinksController {
             }
         }
     }
-
-
-
 }
